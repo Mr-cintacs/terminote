32244eb784882995a91e93c2a0d44295fac7c4a5
Pavit <rocbrainx@gmail.com>
2021-01-23 22:18:57 +0530

    beta ver working

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="id">
+    <div class="container">
+    <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  background-color:#A1B5D8;
+}
+.container{
+  display:flex;
+  flex-direction: column;
+  padding:2em;
+}
+
+</style>
+
