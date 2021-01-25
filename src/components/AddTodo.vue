1b2816a572a34ef907d3fbc4cc7957a9266fd834
pavit kailay <rocbrainx@gmail.com>
2021-01-25 19:07:36 +0530

    'klfjd'

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class='add-todo-container'>
+      <h3 id="heading" >Add Todo</h3>
+      <div class="add">
+          <form v-on:submit="onSubmit">
+              <input type="text" v-model="title" placeholder="Add Todo..">
+              <input type="submit" value="submit">
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: 'AddTodo',
+    data() {
+        return{
+            title: ''
+        }
+    },
+    methods:{
+        ...mapActions(['addTodo']),
+        onSubmit(e) {
+            e.preventDefault();
+            this.addTodo(this.title);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+.add-todo-container{
+    margin-left:4em;
+}
+#heading{
+    font-family: 'Roboto', sans-serif;
+    color:#FFFCF7;
+    font-size:1.8rem;
+}
+input[type="text"] {
+    border:none;
+    padding:1em;
+    background-color:#E4F0D0;
+    border-radius:20px;
+}
+input[type="submit"] {
+    margin-left:0.5em;
+    padding:0.2em;
+    border:none;
+    color:#E4F0D0;
+    font-weight:bolder;
+    font-size:2rem;
+    cursor:pointer;
+    background-color:#A1B5D8;
+
+    /* transition: border 1s ease-in-out; */
+}
+
+</style>
---
+++ src/components/DeleteTodo.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="trash-can" v-on:dragenter="dragEnter" v-on:dragleave="dragLeave" v-on:drop="dragDrop" v-on:dragover.prevent="">
+        <div id="trash-text">
+            <p>T</p>
+            <p>R</p>
+            <p>A</p>
+            <p>S</p>
+            <p>H</p>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: 'DeleteTodo',
+     props:{
+        current_Todo: Number
+    },
+    data() {
+        return {
+            currentTodo: this.current_Todo
+        }
+    },
+   
+    methods:{
+        ...mapActions(['deleteTodo']),
+        dragEnter(e) {
+            e.target.classList.add('hovering');
+        },
+        dragLeave(e) {
+            e.target.classList.remove('hovering');
+        },
+        dragDrop(e) {
+            e.target.classList.remove('hovering');
+            console.log('on dropped called');
+            console.log("this is the todo id" + this.current_Todo);
+            this.deleteTodo(this.current_Todo);
+         }
+    }
+
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+.trash-can {
+    background-color:#F87060;
+    width:10%;
+    height:300px;
+    border-radius:50px;
+    transition: transform 0.25s ease-in-out;
+}
+.trash-can:hover {
+    transform:scale(1.05);
+}
+.hovering {
+    border: 6px #B3A394 dashed;
+}
+#trash-text {
+    color:#CDD7D6;
+    font-family: 'Roboto', sans-serif;
+    margin-top:50px;
+    text-align:center;
+    font-size:2rem;
+}
+
+</style>
